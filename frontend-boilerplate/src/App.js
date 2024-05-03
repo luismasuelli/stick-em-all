fd58dd9dfff9b152001f8bb88be51d308b1a930c
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-03 15:55:26 -0500

    Properly fixing the routed classes.

--- frontend-boilerplate/src/App.js
+++ frontend-boilerplate/src/App.js
@@ -1,12 +1,13 @@
 import Wrapper from "./Wrapping/Wrapper";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from "./Content/Main";
 
 function App() {
   return (
     <Wrapper expectedChainId={80002} expectedChainName={"Polygon Amoy"}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Main />} />
         </Routes>
       </BrowserRouter>
     </Wrapper>
