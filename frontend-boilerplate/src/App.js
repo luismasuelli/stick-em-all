26a36311b49a2a3c5e6747f496efc71fac3fee20
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-03 15:24:12 -0500

    Adding the Router and main entry point.

--- frontend-boilerplate/src/App.js
+++ frontend-boilerplate/src/App.js
@@ -1,9 +1,14 @@
 import Wrapper from "./Wrapping/Wrapper";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Wrapper expectedChainId={80002} expectedChainName={"Polygon Amoy"}>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </Wrapper>
   );
 }
