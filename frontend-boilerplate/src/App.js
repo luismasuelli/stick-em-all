d2504f19be91ac7b7ae6ec651e4cb429973fa9e3
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-04 17:02:00 -0500

    Depending on CHAIN_ID / CHAIN_NAME env. vars.

--- frontend-boilerplate/src/App.js
+++ frontend-boilerplate/src/App.js
@@ -3,8 +3,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from "./Content/Main";
 
 function App() {
+  let chainId = parseInt(process.env.CHAIN_ID);
+  let chainName = process.env.CHAIN_NAME;
+  console.log(`Launching app for chain-id=[${chainId}] chain-name=[${chainName}]`)
   return (
-    <Wrapper expectedChainId={80002} expectedChainName={"Polygon Amoy"}>
+    <Wrapper expectedChainId={chainId} expectedChainName={chainName}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main />} />
