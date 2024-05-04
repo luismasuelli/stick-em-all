3e8e9d578212055516ee031fdb64a97ef33aeb77
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-04 18:34:20 -0500

    Adding the Main UI (far from complete, however).

--- params_frontend/src/Content/Main.js
+++ params_frontend/src/Content/Main.js
@@ -1,13 +1,34 @@
 import {useContext} from "react";
 import {Web3Context} from "web3";
 import Web3AccountContext from "../Wrapping/Web3AccountContext";
+import {AppBar, IconButton, Paper, Toolbar, Typography} from "@mui/material";
+import RefreshIcon from '@mui/icons-material/Refresh';
+import InfoIcon from '@mui/icons-material/Info';
 
 /**
  * This is the main page. All the pages should look like this.
  * @returns {JSX.Element}
  */
 export default function Main() {
-    const context = {...useContext(Web3Context), ...useContext(Web3AccountContext)}
+    const context = {...useContext(Web3Context), ...useContext(Web3AccountContext)};
+    const contractAddress = process.env.REACT_APP_PARAMS_CONTRACT || '0x0';
 
-    return <></>;
+    return <Paper elevation={3} style={{ margin: '40px', marginTop: '120px', padding: '20px' }}>
+        <AppBar position="static" color="primary">
+            <Toolbar>
+                <Typography variant="h6" style={{ flexGrow: 1 }}>
+                    Stick 'em All - Management (Contract: {contractAddress})
+                </Typography>
+                <IconButton color="inherit">
+                    <RefreshIcon />
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+
+        <Typography variant="body1" color="primary" style={{ marginTop: '20px' }}>
+            This section allows the contract owner to modify the business parameters.
+            This involves management of ownership, earnings, and costs (expressed in USD).
+        </Typography>
+        {/* Placeholder for further content */}
+    </Paper>;
 }
