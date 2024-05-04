8837fb30f8663324d60ccae6c6a505dc5088080c
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-04 17:46:11 -0500

    Fixing the environment variables and using them in the App.

--- params_frontend/src/App.js
+++ params_frontend/src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from "./Content/Main";
 
 function App() {
-  let chainId = parseInt(process.env.CHAIN_ID);
-  let chainName = process.env.CHAIN_NAME;
+  let chainId = parseInt(process.env.REACT_APP_CHAIN_ID);
+  let chainName = process.env.REACT_APP_CHAIN_NAME;
   console.log(`Launching app for chain-id=[${chainId}] chain-name=[${chainName}]`)
   return (
     <Wrapper expectedChainId={chainId} expectedChainName={chainName}>
