60758951be80a2c97ed444b89d66078c504c9c82
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-06 18:44:53 -0500

    Adding some formatting to the messages.

--- params_frontend/src/Content/Main.js
+++ params_frontend/src/Content/Main.js
@@ -1,11 +1,12 @@
-import {useContext, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import Web3Context from "../Wrapping/Web3Context";
 import Web3AccountContext from "../Wrapping/Web3AccountContext";
-import {AppBar, IconButton, Paper, Toolbar, Typography} from "@mui/material";
+import {Alert, AppBar, Box, Grid, IconButton, InputLabel, Paper, Toolbar, Typography} from "@mui/material";
 import RefreshIcon from '@mui/icons-material/Refresh';
 import MakeContractClient from './MakeContractClient';
 import InfoIcon from '@mui/icons-material/Info';
 import ErrorLauncherContext from "../Errors/ErrorLauncherContext";
+import AddressInput from "./AddressInput";
 
 /**
  * This is the main page. All the pages should look like this.
@@ -16,18 +17,8 @@ export default function Main() {
     const context = {...useContext(Web3Context), ...useContext(Web3AccountContext)};
     const { web3, account, balanceRefresher } = context;
     const errorLauncher = useContext(ErrorLauncherContext);
-    // Is the contract deployed? Which address?
 
-    const contract = MakeContractClient(web3);
-    const isDeployed = contract !== null;
-    const [ paramsData, setParamsData ] = useState({
-        owner: "0x0",
-        earningsReceiver: "0x0",
-        // eslint-disable-next-line no-undef
-        earningsBalance: BigInt(0),
-        fiatCosts: {}, // Each key is a hash. Each value is an object {native: '0.0', usd: '0.0'}
-    });
-    const isOwner = paramsData.owner === account;
+    // Which are the defined params of the app?
     const params = [
         // Define many costs like this:
         {
@@ -36,6 +27,24 @@ export default function Main() {
         }
     ];
 
+    // Is the contract deployed? Which address?
+    const contract = MakeContractClient(web3);
+    const isDeployed = contract !== null;
+
+    // Which are the retrieved params of our app?
+    const [ paramsData, setParamsData ] = useState({
+        owner: "0x0",
+        earningsReceiver: "0x0",
+        // eslint-disable-next-line no-undef
+        earningsBalance: BigInt(0),
+        fiatCosts: {}, // Each key is a hash. Each value is an object {native: '0.0', usd: '0.0'}
+    });
+    const isOwner = paramsData.owner === account;
+
+    // These state managers stand for our fields:
+    const [ address, setAddress ] = useState('0x0');
+
+    // This refresh function returns EVERYTHING.
     const refresh = errorLauncher.current.capturingError(async function() {
         // First, update the balance.
         await balanceRefresher();
@@ -58,11 +67,16 @@ export default function Main() {
         setParamsData({owner, earningsReceiver, earningsBalance, fiatCosts});
     });
 
+    // Force an initial refresh for our app.
+    useEffect(() => {
+        refresh();
+    }, [refresh]);
+
     return <Paper elevation={3} style={{ margin: '40px', marginTop: '120px', padding: '20px' }}>
         <AppBar position="static" color="primary">
             <Toolbar>
                 <Typography variant="h6" style={{ flexGrow: 1 }}>
-                    Stick 'em All - Management (Contract: {isDeployed ? contract.options.address : "not deployed yet"})
+                    Stick 'em All - Management (Contract: {isDeployed ? contract.options.address + "; Owner: " + paramsData.owner : "not deployed yet"})
                 </Typography>
                 {(isDeployed ? (
                     <IconButton color="inherit" onClick={refresh}>
@@ -72,27 +86,23 @@ export default function Main() {
             </Toolbar>
         </AppBar>
 
-        <Typography variant="body1" color="primary" style={{ marginTop: '20px' }}>
+        <Alert severity="info" sx={{marginTop: 4}}>
             This section allows the contract owner to modify the business parameters.
             This involves management of ownership, earnings, and costs (expressed in USD).
-        </Typography>
+        </Alert>
         {!isOwner ? (
-            <>
-                <Typography variant="body1" color="error">
-                    You're not the owner of this contract. Only the owner of this contract can edit these fields.
-                </Typography>
-                <Typography variant="body1" color="error">
-                    Any attempt to make any change will fail. If you think this is an error / outdated, refresh the page.
-                </Typography>
-            </>
+            <Alert severity="error">
+                You're not the owner of this contract. Only the owner of this contract can edit these fields.<br />
+                Any attempt to make any change will fail. If you think this is an error / outdated, refresh the page.
+            </Alert>
         ) : (
             <>
-                <Typography variant="body1" color="success">
+                <Alert severity="success">
                     You're the owner of this contract. You can change any of these parameters.
-                </Typography>
-                <Typography variant="body1" color="warnings">
+                </Alert>
+                <Alert severity="warning">
                     These actions are dangerous, all of them. These can affect the entire business or make you lose the saved money.
-                </Typography>
+                </Alert>
             </>
         )}
         {/* Placeholder for further content */}
