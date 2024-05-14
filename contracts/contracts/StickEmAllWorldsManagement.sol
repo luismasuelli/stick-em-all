9737fec77f954af8c180aee7100785ec6df11437
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-14 18:05:21 -0500

    Adding more struct definitions. This one is for the page.

--- contracts/contracts/StickEmAllWorldsManagement.sol
+++ contracts/contracts/StickEmAllWorldsManagement.sol
@@ -91,8 +91,10 @@ contract StickEmAllWorldsManagement {
      * sticker can also imply an achievement (and, like they,
      * it has a name and image).
      *
-     * Each album can define up to 2**32 different stickers and
-     * it is NOT forced to use all the stickers' rarities.
+     * Each album can define up to 2**32 - 1 different stickers
+     * and it is NOT forced to use all the stickers' rarities.
+     * The id=0 is used to tell that no sticker is defined in a
+     * given slot.
      */
     struct StickerDefinition {
         /**
@@ -115,10 +117,81 @@ contract StickEmAllWorldsManagement {
         StickerRarity rarity;
 
         /**
-         * The index of the related achievement.
+         * The index of the related achievement. If this value is
+         * 0, this means there's no achievement set (the achievement
+         * 0 is always reserved to the Album, which always has that
+         * implicit achievement - so here is used to say "none").
          */
         uint32 achievementId;
     }
 
+    /**
+     * All the available layouts for a page. This accounts for the number of pages.
+     */
+    enum StickerPageLayout {
+        OneItem,
+        TwoItemsVertical,
+        TwoItemsHorizontal,
+        TwoItemsDiagonalUp,
+        TwoItemsDiagonalDown,
+        ThreeItemsVertical,
+        ThreeItemsHorizontal,
+        ThreeItemsTriangleUp,
+        ThreeItemsTriangleDown,
+        ThreeItemsDiagonalUp,
+        ThreeItemsDiagonalDown,
+        FourItemsSquare,
+        FourItems1Up,
+        FourItems1Down,
+        FiveItemsSquare,
+        FiveItems3Up,
+        FiveItems3Down,
+        SixItems
+    }
 
+    /**
+     * Each sticker page will have one background image (better if 8:9),
+     * a layout, and an OPTIONAL achievement (if zero, then no achievement
+     * is assigned to that particular page). It will also have a name, and
+     * the chosen layout (and, derived, the amount of slots).
+     */
+    struct StickerPageDefinition {
+        /**
+         * The name. It is only for presentational matters.
+         */
+        string name;
+
+        /**
+         * The background image.
+         */
+        string backgroundImage;
+
+        /**
+         * The layout.
+         */
+        StickerPageLayout layout;
+
+        /**
+         * How many stickers can be defined in this page.
+         */
+        uint8 maxStickers;
+
+        /**
+         * How many stickers were defined so far in this page.
+         */
+        uint8 currentlyDefinedStickers;
+
+        /**
+         * The allocated slots, telling their expected stickers.
+         */
+        uint32[] slots;
+
+        /**
+         * The index of the related achievement. If this value is
+         * 0, this means there's no achievement set (the achievement
+         * 0 is always reserved to the Album, which always has that
+         * implicit achievement - so here is used to say "none").
+         */
+        uint32 achievementId;
+    }
 }
