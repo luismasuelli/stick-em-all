87dfb0cecd032c8b71e5405b3843fc88b7350e38
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-19 19:56:21 -0500

    Fixing the names of the deployed worlds and worldsManagement.

--- contracts/ignition/modules/StickEmAllWorlds.js
+++ contracts/ignition/modules/StickEmAllWorlds.js
@@ -3,9 +3,9 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 module.exports = buildModule("StickEmAllWorlds", (m) => {
     const addr = m.getParameter("params", "0x0");
 
-    const mock = m.contract(
+    const worlds = m.contract(
         "StickEmAllWorlds", [addr]
     );
 
-    return { mock };
+    return { worlds };
 });
--- contracts/ignition/modules/StickEmAllWorldsManagement.js
+++ contracts/ignition/modules/StickEmAllWorldsManagement.js
@@ -3,9 +3,9 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 module.exports = buildModule("StickEmAllWorldsManagement", (m) => {
     const addr = m.getParameter("worlds", "0x0");
 
-    const mock = m.contract(
+    const worldsManagement = m.contract(
         "StickEmAllWorldsManagement", [addr]
     );
 
-    return { mock };
+    return { worldsManagement };
 });
