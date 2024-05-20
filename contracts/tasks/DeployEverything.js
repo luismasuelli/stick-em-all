ccb55de8fb54e28452868a435c02ebea9e8439c8
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-19 20:39:19 -0500

    Finishing the migration script.

--- contracts/tasks/DeployEverything.js
+++ contracts/tasks/DeployEverything.js
@@ -4,6 +4,7 @@ const StickEmAllParams = require("../ignition/modules/StickEmAllParams");
 const StickEmAllWorlds = require("../ignition/modules/StickEmAllWorlds");
 const StickEmAllWorldsManagement = require("../ignition/modules/StickEmAllWorldsManagement");
 const VRFCoordinatorV2PlusMock = require("../ignition/modules/VRFCoordinatorV2PlusMock");
+const StickEmAllEconomy = require("../ignition/modules/StickEmAllEconomy");
 const fs = require('fs');
 const path = require('path');
 
@@ -94,12 +95,81 @@ async function deployWorld(hre, paramsAddr) {
 /**
  * Deploys the VRF mock contract.
  * @param hre The hardhat runtime environment.
- * @returns {Promise<{}>} The contract (async function).
+ * @returns {Promise<{vrfAddress, keyHash}>} The contract's address and keyhash (async function).
  */
 async function deployVRF(hre) {
-    const { mock } = await hre.ignition.deploy(VRFCoordinatorV2PlusMock);
-    return mock;
+    switch(hre.network.name) {
+        case "hardhat":
+        case "localhost":
+            const { mock } = await hre.ignition.deploy(VRFCoordinatorV2PlusMock);
+            return { vrfAddress: await mock.getAddress(), keyHash: "0x0" };
+        case "testnet":
+            return {
+                vrfAddress: "0x343300b5d84D444B2ADc9116FEF1bED02BE49Cf2",
+                keyHash: "0x816bedba8a50b294e5cbd47842baf240c2385f2eaf719edbd4f250a137a8c899"
+            };
+        case "mainnet":
+            return {
+                vrfAddress: "0xec0Ed46f36576541C75739E915ADbCb3DE24bD77",
+                keyHash: "0x719ed7d7664abc3001c18aac8130a2265e1e70b7e036ae20f3ca8b92b3154d86"
+            };
+        default:
+            throw new Error("Unknown network: " + hre.network.name);
+    }
 }
+
+
+/**
+ * Deploys the economy and main contracts.
+ * @param hre The hardhat runtime environment.
+ * @param vrfAddress The VRF contract address.
+ * @param worldsManagementAddress The Worlds Management contract address.
+ * @param keyHash The keyHash to use.
+ * @param subscription The subscription to use.
+ * @returns {Promise<{economy, main}>} The economy and main contracts (async function).
+ */
+async function deployMainContracts(hre, vrfAddress, worldsManagementAddress, keyHash, subscription) {
+    const { economy } = await hre.ignition.deploy(StickEmAllEconomy, {
+        parameters: {
+            "StickEmAllEconomy": {
+                "worldsManagement": worldsManagementAddress
+            }
+        }
+    });
+    const economyAddress = await economy.getAddress();
+
+    switch(hre.network.name) {
+        case "hardhat":
+        case "localhost":
+            // Accept subscription as-is.
+            break;
+        case "testnet":
+        case "mainnet":
+            // If subscription is 0 or undefined, error.
+            if (!subscription) {
+                throw new Error("For network " + hre.network.name + ", the subscription argument must be set");
+            }
+            break;
+        default:
+            throw new Error("Unknown network: " + hre.network.name);
+    }
+
+    const { main } = await hre.ignition.deploy(StickEmAllEconomy, {
+        parameters: {
+            "StickEmAllMain": {
+                "economy": economyAddress,
+                "vrf": vrfAddress,
+                "keyHash": keyHash,
+                "subscription": subscription
+            }
+        }
+    });
+
+    await economy.setMainContract(await main.getAddress());
+
+    return { economy, main };
+}
+
 
 /**
  * Computes a keccak256.
@@ -114,7 +184,8 @@ function keccak256(ethers, key) {
 
 task("deploy-everything", "Deploys all our ecosystem")
     .addParam("owner", "The address that will own the ecosystem in the end")
-    .setAction(async ({ owner }, hre, runSuper) => {
+    .addOptionalParam("vrfsub", "The VRF subscription id (mandatory in testnet/mainnet)")
+    .setAction(async ({ owner, vrfsub }, hre, runSuper) => {
         if (!hre.ethers.isAddress(owner)) {
             console.error("The owner is not a valid checksum address");
             return;
@@ -124,15 +195,18 @@ task("deploy-everything", "Deploys all our ecosystem")
         await resetDeployments(hre);
 
         // Deploying or identifying the price feed contract.
+        console.log("Deploying price feed contract...");
         let priceFeedAddr = await deployPriceFeed(hre);
         console.log("Price feed address: " + priceFeedAddr);
 
         // Deploying or identifying the params addr.
+        console.log("Deploying params contract...");
         let params = await deployParams(hre, priceFeedAddr);
         let paramsAddr = await params.getAddress();
         console.log("Params address: " + paramsAddr);
 
         // Deploying the worlds (mint & management) contracts.
+        console.log("Deploying world contracts...");
         let {worlds, worldsManagement} = await deployWorld(hre, paramsAddr);
         let worldsAddress = await worlds.getAddress();
         let worldsManagementAddress = await worldsManagement.getAddress();
@@ -140,9 +214,19 @@ task("deploy-everything", "Deploys all our ecosystem")
         console.log("Worlds Management address: " + worldsManagementAddress);
 
         // Deploying the VRF.
-        let mock = await deployVRF(hre);
-        let mockAddress = await mock.getAddress();
-        console.log("VRF Mock address: " + mockAddress);
+        console.log("Deploying VRF contract...");
+        let { vrfAddress, keyHash } = await deployVRF(hre);
+        console.log("VRF Mock address: " + vrfAddress);
+        console.log("VRF Key hash: " + keyHash);
+
+        console.log("Deploying main/economy contracts...");
+        let { economy, main } = await deployMainContracts(
+            hre, vrfAddress, worldsManagementAddress, keyHash, vrfsub
+        );
+        let economyAddress = await economy.getAddress();
+        let mainAddress = await main.getAddress();
+        console.log("Economy address: " + economyAddress);
+        console.log("Main address: " + mainAddress);
 
         // Define world parameters.
         await worldsAddress.setFiatCost(keccak256("Costs::DefineWorld", 10));
