31a2564c5496b3d92b6d236680292b1d262f37e7
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-22 15:14:47 -0500

    Adding all the events-capturing loop base utilities.

--- worlds_frontend/src/Utils/getEvents.js
+++ worlds_frontend/src/Utils/getEvents.js
@@ -1,11 +1,17 @@
-function defaultUpdateState(s, e) {
+function defaultMutableUpdateState(s, e) {
     s = s || [];
     s.push(e);
     return s;
 }
 
+
 function defaultFinishState(s) {
     return [...s];
+}
+
+
+function defaultImmutableUpdateState(s, e) {
+    return [...s, e];
 }
 
 
@@ -13,7 +19,7 @@ function defaultFinishState(s) {
  * Processes and returns the past events of a contract.
  * @param contract The contract.
  * @param eventNames The names of the events.
- * @param updateState The update state function.
+ * @param updateState The update state function (it can be mutable).
  * @param finishState A finish function to deliver the new state.
  * @param lastBlock The last block.
  * @param lastState The last state.
@@ -24,13 +30,15 @@ async function processPastEvents(contract, eventNames, updateState, finishState,
     let web3 = contract.getContextObject();
     let startBlock = lastBlock == null ? web3.utils.toBN(0) : lastBlock.add(1);
     let state = lastState;
-    updateState = updateState || defaultUpdateState;
+    updateState = updateState || defaultMutableUpdateState;
     finishState = finishState || defaultFinishState;
 
     // Then, for each event, we retrieve the logs.
+    console.log(`Collecting All the past events...`);
     let pastEvents = [];
     let eventNamesLength = eventNames.length;
     for(let idx = 0; idx < eventNamesLength; idx++) {
+        console.log(`>>> Collecting events of type ${eventNames[idx]}...`);
         pastEvents.push(...(await contract.getPastEvents(eventNames[idx], {
             fromBlock: startBlock,
             toBlock: 'latest'
@@ -38,6 +46,7 @@ async function processPastEvents(contract, eventNames, updateState, finishState,
     }
 
     // Re-map and sort the past events.
+    console.log(`Cleaning and preparing the ${pastEvents.length} collected events...`);
     pastEvents = pastEvents.map(e => {
         return {
             name: e.name,
@@ -58,12 +67,97 @@ async function processPastEvents(contract, eventNames, updateState, finishState,
     });
 
     // For each event, process the current state based on it.
+    console.log(`Preparing the state based on the ${pastEvents.length} collected events...`);
     pastEvents.forEach((e) => {
         state = updateState(state, e);
     })
-    state = defaultFinishState(state);
+    state = finishState(state);
 
     // Then, return the new state.
     const newBlock = pastEvents.length ? pastEvents[pastEvents.length - 1].blockNumber : null;
     return {lastBlock: newBlock, lastState: state};
+}
+
+
+/**
+ * Processes and returns the future/incoming events of a contract.
+ * @param contract The contract.
+ * @param eventNames The names of the events.
+ * @param updateState The update state function (IT MUST BE IMMUTABLE).
+ * @param pushState A function used to push the last state. IT SHOULD BE DE-REACTIVIZED.
+ * @param lastBlock The last block.
+ * @param lastState The last state.
+ * @returns {Array} The event objects.
+ */
+function processFutureEvents(contract, eventNames, updateState, pushState, {lastBlock, lastState}) {
+    // First, get interested in the events.
+    let web3 = contract.getContextObject();
+    let events = eventNames.map((en) => contract.events[en]({fromBlock: lastBlock.add(1)}));
+    updateState = updateState || defaultImmutableUpdateState;
+
+    events.forEach((e) => {
+        console.log(`>>> Starting the handler for the ${e} event type...`);
+        e.on('data', (ei) => {
+            lastState = updateState(lastState, {
+                name: ei.name,
+                returnValue: ei.returnValue,
+                blockNumber: web3.utils.toBN(ei.blockNumber),
+                transactionIndex: web3.utils.toBN(ei.transactionIndex),
+                logIndex: web3.utils.toBN(ei.logIndex)
+            });
+            pushState(lastState);
+        })
+    })
+
+    return events;
+}
+
+
+/**
+ * Creates the event-capturing effect. This effect should only depend on the contract,
+ * while all the other references should be non-reactive.
+ * @param contract The contract.
+ * @param eventNames The names of the events.
+ * @param updateInitialState The update state function (it can be mutable).
+ * @param finishInitialState A finish function to deliver the new state.
+ * @param updateNextState The update state function (IT MUST BE IMMUTABLE).
+ * @param pushState A function to push the {lastBlock, lastState} to be managed or rendered later.
+ * @param lastBlock The last processed block.
+ * @param lastState The last processed state.
+ * @returns {function(): function(): void} The effect function (async function).
+ */
+function eventsEffect(
+    contract, eventNames, {updateInitialState, finishInitialState}, updateNextState, pushState, {lastBlock, lastState}
+) {
+    updateNextState = updateNextState || defaultImmutableUpdateState;
+
+    // This function is suitable for an effect.
+    return function() {
+        let wasEffectCanceled = false;
+        let eventHandlers = null;
+
+        async function setup() {
+            const result = await processPastEvents(
+                contract, eventNames, updateInitialState, finishInitialState, {lastBlock, lastState}
+            );
+            console.log("Setting the initial state...");
+            pushState(result);
+            if (!wasEffectCanceled) {
+                console.log("Preparing the collection of future events...");
+                eventHandlers = processFutureEvents(contract, eventNames, updateNextState, pushState, result);
+            }
+        }
+
+        setup().then(() => console.log("Events listening setup is done"));
+
+        return function() {
+            if (eventHandlers !== null) {
+                eventHandlers.forEach((eh, idx) => {
+                    console.log(`>>> Stopping the handler for the ${eventNames[idx]} event type...`);
+                    eh.unsubscribe();
+                });
+            }
+            wasEffectCanceled = true;
+        }
+    }
 }
