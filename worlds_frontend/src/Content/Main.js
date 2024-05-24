3175e9448d72dd849ebadc3d88ea5f700c2c5476
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-24 07:45:54 -0500

    Changing the main content to have the contents based off the params window.

--- worlds_frontend/src/Content/Main.js
+++ worlds_frontend/src/Content/Main.js
@@ -1,8 +1,11 @@
-import {useContext, useEffect, useRef, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import Web3Context from "../Wrapping/Web3Context";
 import Web3AccountContext from "../Wrapping/Web3AccountContext";
 import MakeWorldsContractClients from "./Main/MakeWorldsContractClients";
 import getEventsEffect from "../Utils/getEventsEffect";
+import Web3 from "web3";
+import ParamsAwareContractWindow from "./Windows/ParamsAwareContractWindow";
+import StandaloneMessage from "./Windows/StandaloneMessage";
 
 
 function updateNextState(state, event) {
@@ -10,40 +13,46 @@ function updateNextState(state, event) {
     return [...state, event];
 }
 
+// Which are the defined params of the app?
+const params = [
+    // Define many costs like this:
+    {
+        caption: "Defining a World",
+        hash: Web3.utils.soliditySha3("Costs::DefineWorld")
+    },
+]
+
+
+/**
+ * Renders the main content of the window.
+ * @constructor
+ */
+function MainContent() {}
+
 
 /**
  * This is the main page. All the pages should look like this.
  * @returns {JSX.Element}
  */
 export default function Main() {
-    // 1. Get the context and everything needed to have the references to the contracts.
     const context = {...useContext(Web3Context), ...useContext(Web3AccountContext)};
-    const {account, web3} = context;
+    const {web3, account} = context;
     const [contracts, setContracts] = useState(null);
-    const [events, setEvents] = useState([]);
-    const ref = useRef(null);
-    ref.setEvents = function(events) {
-        setEvents(events);
-    }
-    ref.setContracts = function(contracts) {
-        setContracts(contracts);
-    }
+    const description =
+        "This section allows defining new worlds, transferring ownership of the worlds, and " +
+        "(for the respective world owners) managing the contents / properties of the worlds."
 
     useEffect(() => {
-        MakeWorldsContractClients(web3, account).then(ref.setContracts);
-    }, [account, web3, ref]);
+        MakeWorldsContractClients(web3, account).then(setContracts);
+    }, [web3, account]);
 
-    // 2. Once the contracts are set, we can use them to set up the effects properly.
-    useEffect(() => {
-        if (contracts) {
-            return getEventsEffect(
-                contracts.worldsContract,
-                ["Transfer", "WorldEditionAllowanceChanged"],
-                {}, updateNextState, ref.setEvents,
-                {lastState: []}
-            );
-        }
-    }, [contracts, ref]);
-
-    return <></>;
+    if (contracts) {
+        return <ParamsAwareContractWindow caption={"Stick 'Em All - Worlds"} description={description}
+                                          paramsContract={contracts.params} params={params}
+                                          mainContract={contracts.worlds} showOwner={true}>
+            <MainContent />
+        </ParamsAwareContractWindow>;
+    } else {
+        return <StandaloneMessage title="Loading..." content="Loading the contracts..." />;
+    }
 }
