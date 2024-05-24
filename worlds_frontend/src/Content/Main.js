56d0624e430282a9baf96275bb82a73ae2022185
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-24 10:26:02 -0500

    Starting the logic of the main world-editing window.

--- worlds_frontend/src/Content/Main.js
+++ worlds_frontend/src/Content/Main.js
@@ -1,4 +1,4 @@
-import {useContext, useEffect, useState} from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 import Web3Context from "../Wrapping/Web3Context";
 import Web3AccountContext from "../Wrapping/Web3AccountContext";
 import MakeWorldsContractClients from "./Main/MakeWorldsContractClients";
@@ -6,14 +6,101 @@ import getEventsEffect from "../Utils/getEventsEffect";
 import Web3 from "web3";
 import ParamsAwareContractWindow from "./Windows/ParamsAwareContractWindow";
 import StandaloneMessage from "./Windows/StandaloneMessage";
+import {produce} from "immer";
 
 
-function updateNextState(state, event) {
-    // TODO we'll change this later.
-    return [...state, event];
+/*
+ * The information about worlds will be stored like this:
+ *
+ * 1. List of worlds, either owned or allowed. If a world is owned by the user,
+ *    then it will be marked owned:true. If a world is allowed for the user,
+ *    then it will be marked allowed:true.
+ *
+ *    Owned worlds are marked green. Allowed, but not owned, worlds are marked
+ *    blue instead. Worlds that stop being both owned and allowed are removed
+ *    from this list.
+ *
+ * 2. List of worlds data. This is a local cache that tracks the data of a world.
+ *    This data covers name, description, icon, external URL and validator URL.
+ *    The ownership is refreshed automagically. This data is NOT deleted when
+ *    the world is disowned and disallowed.
+ */
+
+
+/**
+ * Updates a state by a given event.
+ * @param state The state.
+ * @param event The event to update by.
+ * @param account The account to update by.
+ * @returns {*} The new event.
+ * @private
+ */
+function _updateState(state, event, account) {
+    if (event.name === "Transfer") {
+        const {from, to, tokenId} = event.returnValue;
+        if (from === account) {
+            const index = state.eventsIndices[tokenId];
+            if (index === undefined) {
+                state.eventsIndices[tokenId] = state.eventsRelevance.length;
+                state.eventsRelevance.push({owned: true});
+            }
+        }
+        if (to === account) {
+            const index = state.eventsIndices[tokenId];
+            if (index !== undefined) {
+                state.eventsRelevance[index].owned = false;
+            }
+        }
+    } else if (event.name === "WorldEditionAllowanceChanged") {
+        const {worldId, who, allowed} = event.returnValue;
+        if (who === account) {
+            const index = state.eventsIndices[worldId];
+            if (index !== undefined) {
+                state.eventsRelevance[index].allowed = allowed;
+            }
+        }
+    }
+    return state;
 }
 
-// Which are the defined params of the app?
+
+/**
+ * Related to an account, processes an event in an immutable way.
+ * @param state The current state.
+ * @param event The event being processed.
+ * @param account The account.
+ * @returns {unknown} The NEW updated state.
+ */
+function updateAccountDependentNextState(state, event, account) {
+    return produce(state, draft => {
+        _updateState(state, event, account);
+    });
+}
+
+
+/**
+ * Related to an account, processes an event in a mutable way.
+ * @param state The current state.
+ * @param event The event being processed.
+ * @param account The account.
+ * @returns {*} The updated state.
+ */
+function updateAccountDependentInitialState(state, event, account) {
+    return _updateState(state || {eventsIndices: {}, eventsRelevance: []});
+}
+
+
+/**
+ * Clones the state.
+ * @param state The original state.
+ * @returns {*} The resulting state.
+ */
+function finishInitialState(state) {
+    return {...state};
+}
+
+
+// Which are the defined & relevant params of the app?
 const params = [
     // Define many costs like this:
     {
@@ -27,7 +114,28 @@ const params = [
  * Renders the main content of the window.
  * @constructor
  */
-function MainContent() {}
+function MainContent({ contracts, account }) {
+    // 1. We'll make use of the main contracts and track the
+    //    cache of events.
+    const {worlds, worldsManagement} = contracts;
+
+    // 2. Keeping a track of the events cache.
+    const {eventsCache, setEventsCache} = useState({eventsIndices: {}, eventsRelevance: []});
+    const ref = useRef(setEventsCache);
+    ref.current = setEventsCache;
+    useEffect(() => {
+        let updateInitialState = (state, event) => updateAccountDependentInitialState(state, event, account);
+        let updateNextState = (state, event) => updateAccountDependentNextState(state, event, account);
+
+        return getEventsEffect(
+            worlds, ["Transfer", "WorldEditionAllowanceChanged"],
+            {updateInitialState, finishInitialState}, updateNextState, ref.current, null
+        )();
+    }, [worlds, ref, account]);
+
+    // 3. Keeping a track of the data associated to events.
+    const {eventsDataCache, setEventsDataCache} = useState({});
+}
 
 
 /**
@@ -49,7 +157,7 @@ export default function Main() {
     if (contracts) {
         return <ParamsAwareContractWindow caption={"Stick 'Em All - Worlds"} description={description}
                                           paramsContract={contracts.params} params={params}
-                                          mainContract={contracts.worlds} showOwner={true}>
+                                          mainContract={contracts.worldsManagement} showOwner={true}>
             <MainContent />
         </ParamsAwareContractWindow>;
     } else {
