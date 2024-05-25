0dc85b10cea1cfa8a4775ab43272c12c13aea873
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-25 17:59:57 -0500

    Completing the WorldCreated screen.

--- worlds_frontend/src/Content/Main/SubStates/WorldCreated.js
+++ worlds_frontend/src/Content/Main/SubStates/WorldCreated.js
@@ -1,11 +1,45 @@
 import {useParams, useNavigate} from 'react-router-dom';
 import {Grid} from "@mui/material";
 import WorldsList from "../Components/WorldsList";
+import ThemedBox from "../../Controls/ThemedBox";
+import {useEffect} from "react";
 
 export default function WorldCreated({ worldsList, worldsData, newWorldData }) {
     let {worldId} = useParams();
+    let navigate = useNavigate();
+
+    useEffect(() => {
+        let timeout = setTimeout(() => {
+            navigate(`/edit/${worldId.toString()}`);
+            timeout = null;
+        }, 5000);
+
+        return () => {
+            if (timeout) clearTimeout(timeout);
+        };
+    }, [worldId, newWorldData, navigate]);
+
     return <Grid container>
         <Grid item xs={5}><WorldsList worldsList={worldsList} worldsData={worldsData} /></Grid>
-        <Grid item xs={7}></Grid>
+        <Grid item xs={7}>
+            <ThemedBox severity="none" sx={{
+                display: 'flex', alignItems: 'center', justifyContent: 'center',
+                height: '100%', width: '100%', padding: 2
+            }}>
+                <p>The world was successfully created with this id: {worldId.toString()}</p>
+                <p>
+                    Name: {newWorldData.name}<br />
+                    Description: {newWorldData.description}<br />
+                    Logo: {newWorldData.logo}<br />
+                    Background: {newWorldData.background}<br />
+                    External URL: {newWorldData.externalUrl}<br />
+                    Validator URL: {newWorldData.validatorUrl}<br />
+                    Earnings Receiver: {newWorldData.earningsReceiver}
+                </p>
+                <p>
+                    You'll be properly redirected in 5 seconds...
+                </p>
+            </ThemedBox>
+        </Grid>
     </Grid>;
 }
