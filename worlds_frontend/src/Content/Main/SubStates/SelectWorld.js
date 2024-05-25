47b994299afb361d35449dc3053242897030cb7e
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-25 18:03:14 -0500

    Finishing the SelectWorld screen.

--- worlds_frontend/src/Content/Main/SubStates/SelectWorld.js
+++ worlds_frontend/src/Content/Main/SubStates/SelectWorld.js
@@ -1,10 +1,17 @@
-import {useNavigate} from 'react-router-dom';
 import {Grid} from "@mui/material";
 import WorldsList from "../Components/WorldsList";
+import ThemedBox from "../../Controls/ThemedBox";
 
 export default function SelectWorld({ worldsList, worldsData }) {
     return <Grid container>
         <Grid item xs={5}><WorldsList worldsList={worldsList} worldsData={worldsData} /></Grid>
-        <Grid item xs={7}></Grid>
+        <Grid item xs={7}>
+            <ThemedBox severity="none" sx={{
+                display: 'flex', alignItems: 'center', justifyContent: 'center',
+                height: '100%', width: '100%', padding: 2
+            }}>
+                <p>Select or create a world to continue</p>
+            </ThemedBox>
+        </Grid>
     </Grid>;
 }
