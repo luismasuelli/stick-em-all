9d208ee032c72c0fe3633ada503ae4c89d6336d9
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-25 16:22:47 -0500

    Adding the consumption of parameters to the CreateWorld screen.

--- worlds_frontend/src/Content/Main/SubStates/CreateWorld.js
+++ worlds_frontend/src/Content/Main/SubStates/CreateWorld.js
@@ -1,8 +1,17 @@
 import {useNavigate} from 'react-router-dom';
 import {Grid} from "@mui/material";
 import WorldsList from "../Components/WorldsList";
+import {useContext} from "react";
+import ParamsContext from "../../Contexts/ParamsContext";
 
 export default function CreateWorld({ worldsList, worldsData, worldsContract, setNewWorldData }) {
+    const paramsContext = useContext(ParamsContext);
+    const worldDefinitionHash = paramsContext.params[0].hash;
+    const fiatPrice = paramsContext.paramsData.fiatCosts[paramsContext.params[0]];
+    const nativePrice = paramsContext.paramsData.nativeCosts[paramsContext.params[0]];
+
+    // TODO implement this later.
+
     return <Grid container>
         <Grid item xs={5}><WorldsList worldsList={worldsList} worldsData={worldsData} /></Grid>
         <Grid item xs={7}></Grid>
