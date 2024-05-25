9f306083919afdeb56f1345b685b9c1714ad8a75
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-25 18:24:54 -0500

    Making a non-reactive wrapper.

---
+++ worlds_frontend/src/Utils/nonReactive.js
@@ -0,0 +1,13 @@
+import {useRef} from "react";
+
+
+/**
+ * Helper to make a function non-reactive.
+ * @param f The function.
+ * @returns {function(...[*]): *} The non-reactive wrapper.
+ */
+export default function useNonReactive(f) {
+    const ref = useRef(f);
+    ref.current = f;
+    return (...args) => ref.current(...args);
+}
