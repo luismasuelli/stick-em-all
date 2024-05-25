5de8bf1d17ab60d243142995d3be147364c83379
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-25 10:24:32 -0500

    Adding more useNavigate calls.

--- worlds_frontend/src/Content/Main/SubStates/EditWorld.js
+++ worlds_frontend/src/Content/Main/SubStates/EditWorld.js
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 export default function EditWorld() {
     let { worldId } = useParams();
