aef9440524c7d4efae6f1efe182ccc2584448ea2
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-25 12:26:49 -0500

    Adding the exports.

--- params_frontend/src/Content/Controls/ThemedBox.js
+++ params_frontend/src/Content/Controls/ThemedBox.js
@@ -2,7 +2,7 @@ import {useTheme} from "@mui/material/styles";
 import Box from "@mui/material/Box";
 
 
-function ThemedBox({ severity = 'info', sx = {}, children, ...props }) {
+export default function ThemedBox({ severity = 'info', sx = {}, children, ...props }) {
     const theme = useTheme();
     const backgroundColor = theme.palette[severity]?.main; // Get main color based on severity
     const color = theme.palette.getContrastText(backgroundColor);
