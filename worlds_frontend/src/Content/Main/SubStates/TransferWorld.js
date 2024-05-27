302d45e4a377d5de28d7731333e4fd5b88e4d1b5
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-27 17:46:20 -0500

    The transfer section now has capabilities to allow edition.

--- worlds_frontend/src/Content/Main/SubStates/TransferWorld.js
+++ worlds_frontend/src/Content/Main/SubStates/TransferWorld.js
@@ -8,12 +8,16 @@ import Web3AccountContext from "../../../Wrapping/Web3AccountContext";
 import WorldsListEnabledLayout from "../Components/WorldsListEnabledLayout";
 import AddressInput from "../../Controls/AddressInput";
 import Box from "@mui/material/Box";
+import Heading from "../../Controls/Heading";
+import RadioGroupToggle from "../../Controls/RadioGroupToggle";
 
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 export default function TransferWorld({ worldsList, worldsData, worldsContract }) {
     let {worldId} = useParams();
-    let [targetAccount, setTargetAccount] = useState("0x0");
+    let [targetTransferAccount, setTargetTransferAccount] = useState("0x0");
+    let [targetAllowAccount, setTargetAllowAccount] = useState("0x0");
+    let [allowed, setAllowed] = useState(false);
     let {wrappedCall} = useContext(ContractWindowContext);
     const navigate = useNavigate();
     const context = {...useContext(Web3Context), ...useContext(Web3AccountContext)};
@@ -21,31 +25,64 @@ export default function TransferWorld({ worldsList, worldsData, worldsContract }
 
     const transfer = wrappedCall(async function() {
         let isAddress = (
-            web3.utils.toChecksumAddress(targetAccount) !==
+            web3.utils.toChecksumAddress(targetTransferAccount) !==
             web3.utils.toChecksumAddress(ZERO_ADDRESS)
         );
         if (!isAddress) {
             throw new Error("Cannot transfer the world to the zero address");
         }
-        await worldsContract.methods.transferFrom(account, targetAccount, worldId).send({from: account});
+        await worldsContract.methods.transferFrom(account, targetTransferAccount, worldId).send({from: account});
         navigate("/");
+    });
+
+    const allow = wrappedCall(async function() {
+        let isAddress = (
+            web3.utils.toChecksumAddress(targetAllowAccount) !==
+            web3.utils.toChecksumAddress(ZERO_ADDRESS)
+        );
+        if (!isAddress) {
+            throw new Error("Cannot [dis]allow the world edition to the zero address");
+        }
+        await worldsContract.methods.setWorldEditionAllowed(
+            worldId, targetAllowAccount, allowed
+        ).send({from: account});
     });
 
     return <WorldsListEnabledLayout sx={{minHeight: "600px"}} worldsList={worldsList} worldsData={worldsData}>
         <Box sx={{display: 'flex', justifyContent: 'flex-start' , marginBottom: 4}}>
             <Button variant="contained" color="primary" onClick={() => navigate(`/edit/${worldId.toString()}`)}>&#9664; Back</Button>
         </Box>
-        <Alert severity="warning">
-            You're about to transfer the world: {worldId.toString()}. Do this carefully
-            since you're about to lose control over this world.
-        </Alert>
         <Grid container>
-            {/* Name */}
+            <Grid item xs={12}><Heading>Transfer</Heading></Grid>
+            <Grid item xs={12}>
+                <Alert severity="warning" sx={{margin: 1}}>
+                    This section is meant to transfer the world: {worldId.toString()}. Do this carefully
+                    since you're about to lose control over this world.
+                </Alert>
+            </Grid>
             <Grid item xs={3}><Label>Target account:</Label></Grid>
             <Grid item xs={9}>
-                <AddressInput variant="outlined" value={targetAccount} onChange={setTargetAccount} />
+                <AddressInput variant="outlined" value={targetTransferAccount} onChange={setTargetTransferAccount} />
                 <Button disabled={!worldsContract} onClick={transfer}
                         variant="contained" color="primary" size="large">Transfer</Button>
+            </Grid>
+            <Grid item xs={12}><Heading>Allow edition</Heading></Grid>
+            <Grid item xs={12}>
+                <Alert severity="warning" sx={{margin: 1}}>
+                    This section is meant to allow someone as editor for the world: {worldId.toString()}.
+                    Do this carefully since this will give/share control for many administrative features.
+                </Alert>
+            </Grid>
+            <Grid item xs={3}><Label>Target account:</Label></Grid>
+            <Grid item xs={9}>
+                <AddressInput variant="outlined" value={targetAllowAccount} onChange={setTargetAllowAccount} />
+            </Grid>
+            <Grid item xs={12}>
+                <RadioGroupToggle trueLabel="Allow" falseLabel="Disallow" value={allowed} onChange={setAllowed} />
+            </Grid>
+            <Grid item xs={12}>
+                <Button disabled={!worldsContract} onClick={allow}
+                        variant="contained" color="primary" size="large">Update</Button>
             </Grid>
         </Grid>
     </WorldsListEnabledLayout>;
