032dbdc85dbdfe9f0281bda309bb988c11a87272
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-27 13:58:58 -0500

    Ensuring adding a proper stretch.

--- worlds_frontend/src/Content/Main/Components/WorldsListEnabledLayout.js
+++ worlds_frontend/src/Content/Main/Components/WorldsListEnabledLayout.js
@@ -3,17 +3,17 @@ import WorldsList from "./WorldsList";
 import {useNavigate} from "react-router-dom";
 
 
-export default function WorldsListEnabledLayout({ worldsList, worldsData, children, ...props}) {
+export default function WorldsListEnabledLayout({ worldsList, worldsData, children, sx={}, ...props}) {
     const navigate = useNavigate();
 
-    return <Grid container {...props}>
-        <Grid item xs={4} sx={{display: 'flex', flexDirection: 'column', height: "100%"}}>
+    return <Grid container {...props} sx={{...(sx||{}), display: 'flex', alignItems: 'stretch'}}>
+        <Grid item xs={4} sx={{display: 'flex', flexDirection: 'column'}}>
             <WorldsList worldsList={worldsList} worldsData={worldsData} sx={{ flexGrow: 1, overflow: 'auto' }} />
             <Button onClick={() => navigate("/create")} variant="contained" color="primary" size="large">
                 Create
             </Button>
         </Grid>
-        <Grid item xs={8} sx={{height: "100%"}}>
+        <Grid item xs={8} sx={{display: 'flex', flexDirection: 'column'}}>
             {children}
         </Grid>
     </Grid>;
