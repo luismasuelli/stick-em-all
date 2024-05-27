bffd5c5bf0de8635f3a0f10fd6f7c802cb5d0998
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-27 09:06:06 -0500

    Fixing the ThemedBox.

--- frontend_boilerplate/src/Content/Controls/ThemedBox.js
+++ frontend_boilerplate/src/Content/Controls/ThemedBox.js
@@ -2,23 +2,30 @@ import {useTheme} from "@mui/material/styles";
 import Box from "@mui/material/Box";
 
 
-export default function ThemedBox({ severity = 'info', sx = {}, children, ...props }) {
+export default function ThemedBox({
+    severity = 'info', sx = {}, children, useThemeBorderRadius = false, ...props
+}) {
     const theme = useTheme();
-    const backgroundColor = theme.palette[severity]?.main; // Get main color based on severity
-    const color = theme.palette.getContrastText(backgroundColor);
+    let finalSx = {
+        ...sx, padding: theme.spacing(2)
+    };
+    if (useThemeBorderRadius) {
+        finalSx.borderRadius = theme.shape.borderRadius
+    }
+
+    if (severity !== 'none') {
+        const backgroundColor = theme.palette[severity]?.main; // Get main color based on severity
+        const color = theme.palette.getContrastText(backgroundColor);
+        const borderColor = theme.palette[severity]?.dark;
+
+        finalSx = {
+            ...finalSx, backgroundColor, color,
+            border: `1px solid ${borderColor}`,
+        }
+    }
 
     return (
-        <Box
-            sx={{
-                backgroundColor,
-                color,
-                padding: theme.spacing(2),
-                borderRadius: theme.shape.borderRadius,
-                border: `1px solid ${theme.palette[severity]?.dark}`,
-                ...sx
-            }}
-            {...props}
-        >
+        <Box sx={finalSx} {...props}>
             {children}
         </Box>
     );
--- worlds_frontend/src/Content/Controls/ThemedBox.js
+++ worlds_frontend/src/Content/Controls/ThemedBox.js
@@ -2,11 +2,16 @@ import {useTheme} from "@mui/material/styles";
 import Box from "@mui/material/Box";
 
 
-export default function ThemedBox({ severity = 'info', sx = {}, children, ...props }) {
+export default function ThemedBox({
+    severity = 'info', sx = {}, children, useThemeBorderRadius = false, ...props
+}) {
     const theme = useTheme();
     let finalSx = {
-        ...sx, padding: theme.spacing(2), borderRadius: theme.shape.borderRadius,
+        ...sx, padding: theme.spacing(2)
     };
+    if (useThemeBorderRadius) {
+        finalSx.borderRadius = theme.shape.borderRadius
+    }
 
     if (severity !== 'none') {
         const backgroundColor = theme.palette[severity]?.main; // Get main color based on severity
