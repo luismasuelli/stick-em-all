758df9b921ed8c7c32fc06b6aa9027adc07dd573
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-27 09:19:52 -0500

    Adding the Create button to the WorldsListEnabledLayout.js file.

--- worlds_frontend/src/Content/Main/Components/WorldsListEnabledLayout.js
+++ worlds_frontend/src/Content/Main/Components/WorldsListEnabledLayout.js
@@ -1,11 +1,17 @@
-import {Grid} from "@mui/material";
+import {Button, Grid} from "@mui/material";
 import WorldsList from "./WorldsList";
+import {useNavigate} from "react-router-dom";
 
 
 export default function WorldsListEnabledLayout({ worldsList, worldsData, children, ...props}) {
+    const navigate = useNavigate();
+
     return <Grid container {...props}>
-        <Grid item xs={4} sx={{height: "100%"}}>
-            <WorldsList worldsList={worldsList} worldsData={worldsData} />
+        <Grid item xs={4} sx={{display: 'flex', flexDirection: 'column', height: "100%"}}>
+            <WorldsList worldsList={worldsList} worldsData={worldsData} sx={{ flexGrow: 1, overflow: 'auto' }} />
+            <Button onClick={() => navigate("/create")} variant="contained" color="primary" size="large">
+                Create
+            </Button>
         </Grid>
         <Grid item xs={8} sx={{height: "100%"}}>
             {children}
