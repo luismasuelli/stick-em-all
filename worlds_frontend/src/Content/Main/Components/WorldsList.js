ce66ae75e148955dee784e1f0dfd245e05f9b71e
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-28 08:59:46 -0500

    Using an old-style relative/absolute positioning to create the scrollable content.

--- worlds_frontend/src/Content/Main/Components/WorldsList.js
+++ worlds_frontend/src/Content/Main/Components/WorldsList.js
@@ -24,21 +24,23 @@ export default function WorldsList({ worldsList, worldsData, ...props }) {
     return <Box sx={{
         height: '100%',   // Use 100% of the parent height
         width: '100%',    // Use 100% of the parent width
-        overflowY: 'auto', // Show vertical scrollbar when needed
-        flexGrow: 1
+        flexGrow: 1,
+        position: 'relative'
     }}>
-        {worldsList.length ? worldsList.map((world, index) => (
-            <ThemedBox key={index} {...props} severity={world.owned ? "success" : "info"}
-                       onClick={() => handleClick(world.worldId)} style={{userSelect: "none"}}>
-                <h3 style={{textOverflow: "ellipsis", padding: 0, margin: 0}}>ID: {world.worldId.toString()}</h3>
-                {(worldsData[world.worldId]) ? (
-                    <p style={{textOverflow: "ellipsis", padding: 0, margin: 0}}>{worldsData[world.worldId].name}</p>
-                ) : null}
-            </ThemedBox>
-        )) : (
-            <ThemedBox {...props} severity="info">
-                <p style={{textOverflow: "ellipsis", padding: 0, margin: 0}}>No defined worlds</p>
-            </ThemedBox>
-        )}
+        <Box sx={{position: 'absolute', left: 0, top: 0, right: 0, bottom: 0, overflowY: 'auto'}}>
+            {worldsList.length ? worldsList.map((world, index) => (
+                <ThemedBox key={index} {...props} severity={world.owned ? "success" : "info"}
+                           onClick={() => handleClick(world.worldId)} style={{userSelect: "none"}}>
+                    <h3 style={{textOverflow: "ellipsis", padding: 0, margin: 0}}>ID: {world.worldId.toString()}</h3>
+                    {(worldsData[world.worldId]) ? (
+                        <p style={{textOverflow: "ellipsis", padding: 0, margin: 0}}>{worldsData[world.worldId].name}</p>
+                    ) : null}
+                </ThemedBox>
+            )) : (
+                <ThemedBox {...props} severity="info">
+                    <p style={{textOverflow: "ellipsis", padding: 0, margin: 0}}>No defined worlds</p>
+                </ThemedBox>
+            )}
+        </Box>
     </Box>;
 }
