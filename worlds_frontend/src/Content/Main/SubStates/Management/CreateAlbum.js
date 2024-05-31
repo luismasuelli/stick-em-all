4fa68b3b4f8c6d4af19ca96734fa3a76eb1e557f
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-31 12:07:32 -0500

    Starting the features to create/edit/edit-page of an album.

--- worlds_frontend/src/Content/Main/SubStates/Management/CreateAlbum.js
+++ worlds_frontend/src/Content/Main/SubStates/Management/CreateAlbum.js
@@ -1,7 +1,22 @@
-import {useParams, useNavigate} from 'react-router-dom';
-import {Grid} from "@mui/material";
+import AlbumsListEnabledLayout from "../../Components/AlbumsListEnabledLayout";
+import {useParams} from "react-router-dom";
+import {useContext} from "react";
+import Web3Context from "../../../../Wrapping/Web3Context";
+import Web3AccountContext from "../../../../Wrapping/Web3AccountContext";
+import ContractWindowContext from "../../../Contexts/ContractWindowContext";
 
-export default function CreateAlbum({ worldsManagementContract }) {
-    let {worldId} = useParams();
-    return <></>;
+export default function CreateAlbum({
+    worldsManagement, worldsData, albumsCache, albumsDataCache,
+    selectedWorldId, setSelectedWorldId
+}) {
+    const {worldId} = useParams();
+    const context = {...useContext(Web3Context), ...useContext(Web3AccountContext)};
+    const {account} = context;
+    const {wrappedCall} = useContext(ContractWindowContext);
+
+    return <AlbumsListEnabledLayout worldsData={worldsData} albumsData={albumsDataCache}
+                                    selectedWorldId={selectedWorldId} setSelectedWorldId={setSelectedWorldId}
+                                    albumsList={albumsCache.lastState.albumsRelevance}>
+        {/*Content here*/}
+    </AlbumsListEnabledLayout>;
 }
