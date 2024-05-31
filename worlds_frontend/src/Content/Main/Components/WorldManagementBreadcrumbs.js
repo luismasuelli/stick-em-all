ec9579a0c1b7c031f8fdc47975c87b4decc5753c
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-30 21:45:29 -0500

    Adding a component for the breadcrumbs in the management pages.

---
+++ worlds_frontend/src/Content/Main/Components/WorldManagementBreadcrumbs.js
@@ -0,0 +1,16 @@
+import Box from "@mui/material/Box";
+import {Button, Typography} from "@mui/material";
+import React from "react";
+import {useNavigate} from "react-router-dom";
+
+export function WorldManagementBreadcrumbs({ worldId, worldData }) {
+    const navigate = useNavigate();
+
+    return <Box sx={{display: 'flex', justifyContent: 'space-between', marginBottom: 4}}>
+        <Button variant="contained" color="primary"
+                onClick={() => navigate(`/edit/${worldId.toString()}`)}>&#9664; Back</Button>
+        <Typography sx={{p: 2, textAlign: 'right'}}>
+            You're managing the albums for the world {worldId.toString()} - {worldData.name}
+        </Typography>
+    </Box>
+}
