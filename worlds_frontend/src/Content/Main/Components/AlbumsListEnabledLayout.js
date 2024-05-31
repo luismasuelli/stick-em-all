f953c3ee991e473f481e4ef935de4ae84ef933aa
Luis Masuelli <luisfmasuelli@gmail.com>
2024-05-30 21:15:34 -0500

    Adding the list of albums (only the components).

---
+++ worlds_frontend/src/Content/Main/Components/AlbumsListEnabledLayout.js
@@ -0,0 +1,24 @@
+import {Button, Grid} from "@mui/material";
+import AlbumsList from "./AlbumsList";
+import {useNavigate, useParams} from "react-router-dom";
+
+
+export default function AlbumsListEnabledLayout({
+    albumsList, albumsData, children, sx={}, minHeight = "600px", ...props
+}) {
+    const navigate = useNavigate();
+    let {worldId} = useParams();
+
+    return <Grid container {...props} sx={{...(sx||{}), alignItems: "stretch", minHeight}}>
+        <Grid item xs={4} sx={{display: 'flex', flexDirection: 'column', padding: 2}}>
+            <AlbumsList albumsList={albumsList} albumsData={albumsData} />
+            <Button onClick={() => navigate("/manage/" + worldId.toString() + "/create")}
+                    variant="contained" color="primary" size="large">
+                Create Album
+            </Button>
+        </Grid>
+        <Grid item xs={8} sx={{display: 'flex', flexDirection: 'column', padding: 2}}>
+            {children}
+        </Grid>
+    </Grid>;
+}
--- worlds_frontend/src/Content/Main/Components/WorldsListEnabledLayout.js
+++ worlds_frontend/src/Content/Main/Components/WorldsListEnabledLayout.js
@@ -3,14 +3,16 @@ import WorldsList from "./WorldsList";
 import {useNavigate} from "react-router-dom";
 
 
-export default function WorldsListEnabledLayout({ worldsList, worldsData, children, sx={}, minHeight = "600px", ...props}) {
+export default function WorldsListEnabledLayout({
+    worldsList, worldsData, children, sx={}, minHeight = "600px", ...props
+}) {
     const navigate = useNavigate();
 
     return <Grid container {...props} sx={{...(sx||{}), alignItems: "stretch", minHeight}}>
         <Grid item xs={4} sx={{display: 'flex', flexDirection: 'column', padding: 2}}>
             <WorldsList worldsList={worldsList} worldsData={worldsData} />
             <Button onClick={() => navigate("/create")} variant="contained" color="primary" size="large">
-                Create
+                Create World
             </Button>
         </Grid>
         <Grid item xs={8} sx={{display: 'flex', flexDirection: 'column', padding: 2}}>
