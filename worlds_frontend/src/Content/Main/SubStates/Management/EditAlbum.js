10cb85f791b501b51c5f6d1050dbb99cdb7618e6
Luis Masuelli <luisfmasuelli@gmail.com>
2024-06-01 11:31:50 -0500

    Starting the EditAlbum section.

--- worlds_frontend/src/Content/Main/SubStates/Management/EditAlbum.js
+++ worlds_frontend/src/Content/Main/SubStates/Management/EditAlbum.js
@@ -1,22 +1,24 @@
 import AlbumsListEnabledLayout from "../../Components/AlbumsListEnabledLayout";
 import {useParams} from "react-router-dom";
-import {useContext, useEffect} from "react";
+import {useContext, useEffect, useState} from "react";
 import Web3Context from "../../../../Wrapping/Web3Context";
 import Web3AccountContext from "../../../../Wrapping/Web3AccountContext";
 import ContractWindowContext from "../../../Contexts/ContractWindowContext";
 
-export default function EditAlbum({
-    worldsManagement, worldsData, albumsCache, albumsDataCache,
-    selectedWorldId, setSelectedWorldId, setAlbumData
-}) {
-    const {worldId, albumId} = useParams();
+function useGlobalContextData() {
     const context = {...useContext(Web3Context), ...useContext(Web3AccountContext)};
     const {account} = context;
+    const {worldId, albumId} = useParams();
     const {wrappedCall} = useContext(ContractWindowContext);
-    const albumData = albumsDataCache[albumId];
+
+    return {account, worldId, albumId, wrappedCall};
+}
+
+function AlbumData({ worldsManagement, setAlbumData }) {
+    const {wrappedCall, albumId} = useGlobalContextData();
 
     useEffect(() => {
-        const getWorldData = wrappedCall(async function getWorldData() {
+        const getAlbumData = wrappedCall(async function getWorldData() {
             // 1. Download the album data for the given id.
             const {
                 worldId, name, edition, frontImage, backImage, rarityIcons,
@@ -29,12 +31,60 @@ export default function EditAlbum({
             // 2. Set the downloaded world's data into the worldsData for the worldId.
             setAlbumData(albumId, retrievedAlbumData);
         });
-        getWorldData();
+        getAlbumData();
     }, [albumId, wrappedCall, worldsManagement, setAlbumData]);
+}
+
+function AlbumPages({ worldsManagement }) {
+    // Global contexts.
+    const {wrappedCall, albumId, worldId, account} = useGlobalContextData();
+
+    // Local data.
+    const [albumPages, setAlbumPages] = useState([]);
+}
+
+function AlbumAchievements({ worldsManagement }) {
+    // Global contexts.
+    const {wrappedCall, albumId, worldId, account} = useGlobalContextData();
+
+    // Local data.
+    const [albumAchievements, setAlbumAchievements] = useState([]);
+}
+
+function AlbumBoosterPackRules({ worldsManagement }) {
+    // Global contexts.
+    const {wrappedCall, albumId, worldId, account} = useGlobalContextData();
+
+    // Local data.
+    const [albumBoosterPackRules, setAlbumBoosterPackRules] = useState([]);
+}
+
+function AlbumReleasePreview({ worldsManagement }) {
+    // Global contexts.
+    const {wrappedCall, albumId, worldId, account} = useGlobalContextData();
+
+    // Local data.
+    const [albumBoosterPackRules, setAlbumBoosterPackRules] = useState([]);
+}
+
+export default function EditAlbum({
+    worldsManagement, worldsData, albumsCache, albumsDataCache,
+    selectedWorldId, setSelectedWorldId, setAlbumData
+}) {
+    const {worldId, albumId} = useParams();
+    const albumData = albumsDataCache[albumId];
 
     return <AlbumsListEnabledLayout worldsData={worldsData} albumsData={albumsDataCache}
                                     selectedWorldId={selectedWorldId} setSelectedWorldId={setSelectedWorldId}
                                     albumsList={albumsCache.lastState.albumsRelevance}>
-        {/*Content here*/}
+        <AlbumData worldsManagement={worldsManagement} worldId={worldId} albumId={albumId}
+                   setAlbumData={setAlbumData} />
+        <AlbumPages worldsManagement={worldsManagement} worldId={worldId} albumId={albumId} />
+        <AlbumAchievements worldsManagement={worldsManagement} worldId={worldId} albumId={albumId} />
+        {albumData ? (
+            albumData.released ?
+                <AlbumBoosterPackRules worldsManagement={worldsManagement} /> :
+                <AlbumReleasePreview worldsManagement={worldsManagement} />
+        ) : null}
     </AlbumsListEnabledLayout>;
 }
