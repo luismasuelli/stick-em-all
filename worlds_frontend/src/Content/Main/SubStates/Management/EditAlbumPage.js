3644c0f2b240755d8d76e233f2ebab7ca6d44e35
Luis Masuelli <luisfmasuelli@gmail.com>
2024-06-01 22:40:36 -0500

    Starting the page settings.

--- worlds_frontend/src/Content/Main/SubStates/Management/EditAlbumPage.js
+++ worlds_frontend/src/Content/Main/SubStates/Management/EditAlbumPage.js
@@ -1,9 +1,11 @@
 import AlbumsListEnabledLayout from "../../Components/AlbumsListEnabledLayout";
-import {useParams} from "react-router-dom";
-import {useContext} from "react";
+import {useNavigate, useParams} from "react-router-dom";
+import React, {useContext} from "react";
 import Web3Context from "../../../../Wrapping/Web3Context";
 import Web3AccountContext from "../../../../Wrapping/Web3AccountContext";
 import ContractWindowContext from "../../../Contexts/ContractWindowContext";
+import {Button, Typography} from "@mui/material";
+import Box from "@mui/material/Box";
 
 export default function EditAlbumPage({
     worldsManagement, worldsData, albumsCache, albumsDataCache,
@@ -13,10 +15,21 @@ export default function EditAlbumPage({
     const context = {...useContext(Web3Context), ...useContext(Web3AccountContext)};
     const {account} = context;
     const {wrappedCall} = useContext(ContractWindowContext);
+    const navigate = useNavigate();
+
+    const albumData = albumsDataCache[albumId];
+    const worldData = worldsData[worldId];
+    const url = `/manage/${worldId.toString()}/edit/${albumId.toString()}`;
 
     return <AlbumsListEnabledLayout worldsData={worldsData} albumsData={albumsDataCache}
                                     selectedWorldId={selectedWorldId} setSelectedWorldId={setSelectedWorldId}
                                     albumsList={albumsCache.lastState.albumsRelevance}>
-        {/*Content here*/}
+        <Box sx={{display: 'flex', justifyContent: 'space-between', marginBottom: 4}}>
+            <Button variant="contained" color="primary"
+                    onClick={() => navigate(url)}>&#9664; Back</Button>
+            <Typography sx={{p: 2, textAlign: 'right'}}>
+                You're managing the album {albumId.toString()} - {albumData.name} for the world {worldId.toString()} - {worldData.name}
+            </Typography>
+        </Box>
     </AlbumsListEnabledLayout>;
 }
