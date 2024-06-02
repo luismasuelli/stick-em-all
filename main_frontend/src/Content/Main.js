dd4cb8c4695c529764f13f0fc1215202afd18b37
Luis Masuelli <luisfmasuelli@gmail.com>
2024-06-02 13:39:06 -0500

    Starting the empty barebones of the app.

--- main_frontend/src/Content/Main.js
+++ main_frontend/src/Content/Main.js
@@ -1,13 +1,41 @@
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import Web3Context from "../Wrapping/Web3Context";
 import Web3AccountContext from "../Wrapping/Web3AccountContext";
+import ParamsAwareContractWindow from "./Windows/ParamsAwareContractWindow";
+import StandaloneMessage from "./Windows/StandaloneMessage";
+import mainContractClients from "./Main/mainContractClients";
+
+
+/**
+ * Renders all the content to the end user(s).
+ */
+function MainContent() {
+
+}
+
 
 /**
  * This is the main page. All the pages should look like this.
  * @returns {JSX.Element}
  */
 export default function Main() {
-    const context = {...useContext(Web3Context), ...useContext(Web3AccountContext)}
+    const context = {...useContext(Web3Context), ...useContext(Web3AccountContext)};
+    const {web3, account} = context;
+    const [contracts, setContracts] = useState(null);
+    const description =
+        "You can buy new or make use of your existing albums :D. Enjoy the experience!"
 
-    return <></>;
+    useEffect(() => {
+        mainContractClients(web3, account).then(setContracts);
+    }, [web3, account]);
+
+    if (contracts) {
+        return <ParamsAwareContractWindow caption={"Stick 'Em All"} description={description}
+                                          paramsContract={contracts.params} params={[]}
+                                          mainContract={contracts.worldsManagement}>
+            <MainContent account={account} contracts={contracts} />
+        </ParamsAwareContractWindow>;
+    } else {
+        return <StandaloneMessage title="Loading..." content="Loading the contracts..." />;
+    }
 }
